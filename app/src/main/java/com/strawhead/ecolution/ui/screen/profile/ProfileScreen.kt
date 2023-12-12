7476528d2cbc687cd0874ef46d5dc5a6cb226a96
bluelaned <felixjosua00@gmail.com>
2023-12-13 02:20:24 +0700

    modify : the ProfileScreen & ProfileSignInScreen

--- app/src/main/java/com/strawhead/ecolution/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/com/strawhead/ecolution/ui/screen/profile/ProfileScreen.kt
@@ -1,9 +1,6 @@
 package com.strawhead.ecolution.ui.screen.profile
 
-import android.annotation.SuppressLint
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,31 +8,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import com.strawhead.ecolution.R
-import com.strawhead.ecolution.signin.SignInState
 import com.strawhead.ecolution.signin.UserData
 
 @Composable
@@ -44,11 +31,13 @@ fun ProfileScreen(
     onSignOut: () -> Unit
 ) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        if(userData?.profilePictureUrl != null) {
+        if (userData?.profilePictureUrl != null) {
             AsyncImage(
                 model = userData.profilePictureUrl,
                 contentDescription = "Profile picture",
@@ -59,20 +48,27 @@ fun ProfileScreen(
             )
             Spacer(modifier = Modifier.height(16.dp))
         }
-        if(userData?.username != null) {
+        if (userData?.username != null) {
             Text(
                 text = userData.username,
                 textAlign = TextAlign.Center,
-                fontSize = 36.sp,
-                fontWeight = FontWeight.SemiBold
+                fontSize = 30.sp,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .widthIn(max = 300.dp)
+                    .padding(vertical = 16.dp),
+                style = TextStyle(
+                    lineHeight = 36.sp
+                )
             )
-            Spacer(modifier = Modifier.height(16.dp))
         }
         Button(onClick = onSignOut) {
             Text(text = "Sign out")
         }
     }
 }
+
 
 //@Preview(showBackground = true)
 //@Composable
--- app/src/main/java/com/strawhead/ecolution/ui/screen/profile/ProfileSignInScreen.kt
+++ app/src/main/java/com/strawhead/ecolution/ui/screen/profile/ProfileSignInScreen.kt
@@ -1,17 +1,30 @@
 package com.strawhead.ecolution.ui.screen.profile
 
 import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.strawhead.ecolution.R
 import com.strawhead.ecolution.signin.SignInState
 
 @Composable
@@ -33,11 +46,51 @@ fun ProfileSignInScreen(
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        contentAlignment = Alignment.Center
+            .padding(16.dp)
     ) {
-        Button(onClick = onSignInClick) {
-            Text(text = "Sign in")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = null,
+                tint = colorResource(id = R.color.green)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Welcome Back!",
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "Sign in to continue",
+                color = Color.Gray
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            Button(
+                onClick = onSignInClick,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp)
+            ) {
+                Text(text = "Sign in")
+            }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfileSignInScreenPreview() {
+    val defaultSignInState = SignInState()
+
+    ProfileSignInScreen(
+        state = defaultSignInState,
+        onSignInClick = {}
+    )
 }
